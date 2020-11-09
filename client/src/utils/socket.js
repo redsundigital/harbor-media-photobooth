ef88c287f6078549e4628d29153808de5b430aa1
jason-michael <michael.jason@outlook.com>
2020-11-09 14:05:24 -0600

    remove localhost from client socket host

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001');
+const socket = io(); //'http://localhost:3001'
 
 socket.on('connect', () => {
   console.log('socket connected:', socket.id);
